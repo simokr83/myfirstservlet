d6b26ee28cfbd3ecd50acf6a54f5a0d96187005b
s.megna <s.megna@LAP-264.esse.local>
2017-03-20 17:13:39 +0100

    add ScarpaResource and VestitoResource

--- MagazzinoSrv/src/main/java/it/simona/magazzinosrv/BorsaResource.java
+++ MagazzinoSrv/src/main/java/it/simona/magazzinosrv/BorsaResource.java
@@ -5,6 +5,8 @@
  */
 package it.simona.magazzinosrv;
 
+import it.simona.magazzino.Borsa;
+import java.util.ArrayList;
 import javax.json.Json;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -26,16 +28,14 @@ public class BorsaResource {
     //quello che arriva trasformalo in un oggetto UriInfo di nome context e rappresenta il contesto della chiamata fatta dal client
      @Context
      private UriInfo context;
-     
+     private int id;
     public BorsaResource(){
     }
     
     public BorsaResource(Object [] obj){
-        
-    }
-    
-   
-    
+        obj=new Borsa[id];
+     
+        }
     @GET
     //prende l id che arriva sulla risorsa dalla chiamata http 
     @Path("{id}")
@@ -43,8 +43,14 @@ public class BorsaResource {
     //prendo l id dall url e lo trasformo nel paramentro di tipo stringa
     public String getBorsa(@PathParam("id") String id){
         String result = "{\"id\":123,\"colore\":\"giallo\"}";
+    // for(int i=0; i < obj.length; i++)
+    //   if(obj.item(i).getId(“id”))
+   //cella = lista_td.item(i);
+            
         //andare a prendere dall'array di borse la borsa con id ID
         System.out.println("GET/borsa/"+id);
+        
+      
         return result;
     }
     
